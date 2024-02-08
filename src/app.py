ad14e57a196ccccc5db311e548d07b4dd5f8ea34
Nicolas Jaramillo <nickjcoder@gmail.com>
2024-02-08 19:46:44 +0000

    save

--- src/app.py
+++ src/app.py
@@ -8,8 +8,10 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet
 #from models import Person
+
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -44,8 +46,74 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+#ENDPOINTS DE PEOPLE
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all() # [<Note 1>, <Note 2>]
+    print(people)
+    people = list(map(lambda people: people.serialize(), people))
+    print(people) 
+    return jsonify(people), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id(people_id):
+    people = People.query.get(people_id) # [<Note 1>, <Note 2>]
+    if people is None:
+        return jsonify({'error': 'Person not found'}), 404
+    serialized_person = people.serialize()
+    return jsonify(serialized_person), 200
+
+@app.route('/people', methods=["POST"])
+def add_people():
+    data = request.json
+    name = data.get('name')
+    height = data.get('height')
+    gender = data.get('gender')
+    people = People(name=name, height=height, gender=gender)
+    db.session.add(people) 
+    db.session.commit()
+
+    return jsonify({"message": "People added successfully"}), 201
+
+#/ENDPOINTS DE PEOPLE
+
+
+#ENDPOINTS DE PLANETS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planet = Planet.query.all() # [<Note 1>, <Note 2>]
+    print(planet)
+    planet = list(map(lambda planet: planet.serialize(), planet))
+    print(planet) 
+    return jsonify(planet), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet_id(planet_id):
+    planet = Planet.query.get(planet_id) # [<Note 1>, <Note 2>]
+    if planet is None:
+        return jsonify({'error': 'Planet not found'}), 404
+    serialized_planet = planet.serialize()
+    return jsonify(serialized_planet), 200
+
+@app.route('/planets', methods=["POST"])
+def add_planet():
+    data = request.json
+    name = data.get('name')
+    climate = data.get('climate')
+    terrain = data.get('terrain')
+    planet = Planet(name=name, climate=climate, terrain=terrain)
+    db.session.add(planet) 
+    db.session.commit()
+
+    return jsonify({"message": "Planet added successfully"}), 201
+
+
+
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -7,7 +9,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +19,43 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer)
+    gender = db.Column(db.String(50))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "gender": self.gender,
+            
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            
+        }
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
